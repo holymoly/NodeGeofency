8d0828f7804e77b325434e050ced02d77fec5f4c
Maurice Streubel <holzmolz@googlemail.com>
2014-11-15 17:30:27 +0100

    added basic auth

--- server.js
+++ server.js
@@ -10,6 +10,18 @@ var app = express();
 
 // all environments
 app.use(bodyParser.urlencoded({ extended: true }));
+// use basic authentication
+app.use(function(req, res, next) {
+    var user = auth(req);
+
+    if (user === undefined || user['name'] !== config.authUser || user['pass'] !== config.authPass) {
+        res.statusCode = 401;
+        res.setHeader('WWW-Authenticate', 'Basic realm="NodeGeofency"');
+        res.end('Unauthorized');
+    } else {
+        next();
+    }
+});
 
 app.post('/time', time.receive);
 
