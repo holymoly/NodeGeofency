bc6024494205d9d6161fa85f5c11cd5ad8abe9ec
Maurice Streubel <holzmolz@googlemail.com>
2014-11-15 10:33:52 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,6 @@
 
 var express = require('express');
 var time = require('./routes/time');
-var http = require('http');
-var path = require('path');
 var bodyParser   = require('body-parser');
 var errorhandler = require('errorhandler')
 var port     = process.env.PORT || 8765;
